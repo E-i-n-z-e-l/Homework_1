5f847e5501f132fe18c5a281e7d2b8c46f750e3e
Einzel <einzel0102@mail.ru>
2022-12-19 16:19:14 +0800

    Задача 6 готова

---
+++ Task_6/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int remainder = (number % 2);
+
+if(remainder == 0)
+{
+    Console.WriteLine(number+" четное");
+}
+else
+{
+    Console.WriteLine(number+" не четное");
+}
